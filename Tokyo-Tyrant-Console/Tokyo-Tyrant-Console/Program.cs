6117f73825af04657881f034bb41a1dc4a27a15d
Oleg Demchenko <demchenko.oleg@gmail.com>
2011-08-21 19:28:48 -0700

    *Add Console project

---
+++ Tokyo-Tyrant-Console/Tokyo-Tyrant-Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tokyo_Tyrant_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tokyo Tyrant Console");
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
