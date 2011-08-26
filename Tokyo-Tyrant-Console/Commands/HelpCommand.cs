d32504fedfef1bfb2fa50eab7d22d7807e649a51
Oleg Demchenko <demchenko.oleg@gmail.com>
2011-08-26 10:13:40 -0700

    * Update help command

--- Tokyo-Tyrant-Console/Commands/HelpCommand.cs
+++ Tokyo-Tyrant-Console/Commands/HelpCommand.cs
@@ -14,9 +14,16 @@ namespace Tokyo_Tyrant_Console.Commands
 
         public void Invoke(CommandOptions options)
         {
-            _outputReporter.Report("Get Key: ");    
-            _outputReporter.Report("--get-key keyValue");    
+            _outputReporter.Report("Find by key: ");    
+            _outputReporter.Report("--findby-key keyValue");    
             _outputReporter.Report("");
+
+            _outputReporter.Report("Find by columns: ");
+            _outputReporter.Report("--findby-columns columnName1:columnValue1 [columnName2:columnValue2]");
+            _outputReporter.Report("*Note: All column/value pairs must match in order for the key to be returned");
+            _outputReporter.Report("*Column values are matched using exact string equality");
+            _outputReporter.Report("");
+
 
             _outputReporter.Report("Delete Key: ");
             _outputReporter.Report("--delete-key keyValue");
